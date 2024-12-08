98cdb677a2b3318c09390bfe3e06d612ff9c64a2
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-12-08 15:26:02 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,20 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        if name is None:
+            name = "Default"
+
+        if name == "":
+            print("Name must be string between 1 and 25 characters.")
+        elif not isinstance(name, str):
+            print("Name must be string between 1 and 25 characters.")
+        elif len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+
+        if breed and breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,19 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Default", job=""):
+        if not isinstance(name, str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            self.name = None  
+ 
+        else:
+            self.name = name.title()  
+            
+            
+            if job not in APPROVED_JOBS:
+                print("Job must be in list of approved jobs.")
+                self.job = None  
+            else:
+                self.job = job
+        
+
